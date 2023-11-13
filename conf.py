a0d0fdaf45d7c4a57279c2be75a974252687ee6a
MoltonMontro <montro.l.nakuta@gmail.com>
2023-11-13 15:37:04 -0600

    CSS for table wordwrap

--- conf.py
+++ conf.py
@@ -41,5 +41,13 @@ html_theme_options = {
     'collapse_navigation': True,
 }
 
+# These folders are copied to the documentation's HTML output
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
